fee63657e6c2d9bb284191fdb451ff3d95e445dd
Prazual Karki <prazualkarki@gmail.com>
2023-05-11 10:07:46 +0545

    form validation changes

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,109 +1,133 @@
-import React from 'react';
-import { useState} from 'react';
-import "./LoginPage.css"
+import React from 'react'
+import { useState } from 'react'
+import './LoginPage.css'
 
+export default function LoginPage({ checkLogged }) {
+  const [inputText, setInputText] = useState({
+    email: '',
+    password: '',
+  })
+  const [formErrors, setFormErrors] = useState({})
+  const [isSubmit, setIsSubmit] = useState(false)
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setFormErrors(validate(inputText))
+    setIsSubmit(true)
+    checkLogged(isSubmit)
+  }
+  //   useEffect(() => {
 
+  //     if(Object.keys(formErrors).length === 0 && isSubmit){
+  //         console.log(inputText)
 
-export default function LoginPage({checkLogged}) {
+  //     }
+  //   }, [formErrors]);
+  const validate = (values) => {
+    const errors = {}
 
-    const [inputText,setInputText]=useState({ 
-        
-        email:"",
-        password:""
-    });
-    const [formErrors,setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
-    
-    
-      const submitHandler = (e) => {
-       e.preventDefault();
-       setFormErrors(validate(inputText));
-       setIsSubmit(true);
-       checkLogged(isSubmit);
-      
-          
-      };
-    //   useEffect(() => {
-        
-    //     if(Object.keys(formErrors).length === 0 && isSubmit){
-    //         console.log(inputText)
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)) {
+      errors.email = 'invalid email address'
+    }
+    if (!values.password && values.password.length < 6) {
+      errors.password = 'must be greater than 6 character'
+    }
+    return errors
+  }
 
-
-    //     }
-    //   }, [formErrors]);
-      const validate = (values)=>{
-        const errors = {};
-        
-        
-        if (!values.email){
-            errors.email = "email is required"
-        } 
-        if (!values.password){
-            errors.password = "password is required"
-        } else if (values.password.length < 8){
-            errors.password = "password must be 8 characters long"
-        }
-        return errors;
-
-      }
-        
-        
-      
-      const inputEvent=(event)=>{
-            
-        const name=event.target.name; 
-        const value=event.target.value;
-        setInputText({ ...inputText, [name]: value});
-      } 
+  const inputEvent = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+    setInputText({ ...inputText, [name]: value })
+  }
   return (
     <div className='main'>
-    
       <div className='lheader'>
-        <h1 style={{color:"#287be8", fontSize:"58px",marginBottom:"0px",paddingBottom:"0px"}}>facebook</h1>
-        <p id='lastpara' style={{fontSize:"28px"}}>Connect with friends and the world<br/> around you on Facebook</p>
+        <h1
+          style={{
+            color: '#287be8',
+            fontSize: '58px',
+            marginBottom: '0px',
+            paddingBottom: '0px',
+          }}
+        >
+          facebook
+        </h1>
+        <p id='lastpara' style={{ fontSize: '28px' }}>
+          Connect with friends and the world
+          <br /> around you on Facebook
+        </p>
       </div>
-      <div className='lform' >
-      
+      <div className='lform'>
         <form onSubmit={submitHandler}>
-            <div>
+          <div>
             <label htmlFor='email'></label>
-            <input id='email' type="email" name="email" placeholder="Email or phone number"
-              autoComplete='off' value={inputText.email} onChange={inputEvent}
-            /><br/>
-            <p style={{color:" #FF0000"}}>{formErrors.email}</p>
-            </div>
-            
+            <input
+              id='email'
+              type='email'
+              name='email'
+              placeholder='Email or phone number'
+              autoComplete='off'
+              value={inputText.email}
+              onChange={inputEvent}
+            />
+            <br />
+            <p style={{ color: ' #FF0000' }}>{formErrors.email}</p>
+          </div>
 
-            <div>
+          <div>
             <label htmlFor='password'></label>
-            <input id='password' type="password" name="password" placeholder="Password"
-                autoComplete='off' value={inputText.password} onChange={inputEvent}
-            /><br/>
-            <p style={{color:" #FF0000"}}>{formErrors.password}</p>
-            </div>
-            
-            
-            <div>
-            
-            <button id='login' type="submit">Log In</button><br/><br/>
-            </div>
-            
+            <input
+              id='password'
+              type='password'
+              name='password'
+              placeholder='Password'
+              autoComplete='off'
+              value={inputText.password}
+              onChange={inputEvent}
+            />
+            <br />
+            <p style={{ color: ' #FF0000' }}>{formErrors.password}</p>
+          </div>
 
-            <a href='#' style={{color:"#287be8", textDecoration:"none"}}>Forget password?</a>
+          <div>
+            <button id='login' type='submit'>
+              Log In
+            </button>
+            <br />
+            <br />
+          </div>
 
-            <br/><br/>
-            <hr size='1' style={{opacity:"0.3"}}/>
-            <br/>
+          <a href='#' style={{ color: '#287be8', textDecoration: 'none' }}>
+            Forget password?
+          </a>
 
-            <div>
+          <br />
+          <br />
+          <hr size='1' style={{ opacity: '0.3' }} />
+          <br />
+
+          <div>
             <button id='create'>Create new account</button>
-            </div>
-            
-
+          </div>
         </form>
-        <br/>
-        <p id='lastpara' style={{fontSize:"14px",marginLeft:"8%",fontWeight:"490"}}><a href='#' style={{textDecoration:"none",color:"black",fontWeight:"640"}}>Create a Page</a> for a celebrity, brand or business</p>
+        <br />
+        <p
+          id='lastpara'
+          style={{ fontSize: '14px', marginLeft: '8%', fontWeight: '490' }}
+        >
+          <a
+            href='#'
+            style={{
+              textDecoration: 'none',
+              color: 'black',
+              fontWeight: '640',
+            }}
+          >
+            Create a Page
+          </a>{' '}
+          for a celebrity, brand or business
+        </p>
       </div>
     </div>
   )
